c5354f22d637fa96c69c9ba4ab6a5870336df7ba
Kiba Gateaux <kibagateaux@gmail.com>
2017-01-31 22:47:19 -0500

    adds seperate projects button

--- index.js
+++ index.js
@@ -22,20 +22,9 @@ $(document).ready(function(){
 
   const projects = [
     {
-      head: 'Telescope',
-      site: "https://dry-hollows-37710.herokuapp.com/login",
-      desc: `A game based on Hot or Not for your narcissistic self.
-             Never know which picture to post to Facebook or Instagram?
-             Use Telescope to have your pictures voted on so you know
-             which one to post and get the most likes.
-             You upload the photos of yourself you want voted on to Imgur
-             and we pull them using Imgur's API.
-             View your profile to see your most liked and post it straight away.`,
-      skills: [RubyonRails, postgres, materialUI]
-    },
-    {
       head: 'Kireiki',
       site: "https://github.com/kibagateaux/KiReiKi-Native",
+      github: "https://github.com/kibagateaux/KiReiKi-Native",
       time: '1 week project',
       desc: `Using React Native and Rails API to build a mobile app
              centered on mens fashion. An adaption of Reddit, you can
@@ -46,12 +35,26 @@ $(document).ready(function(){
     {
       head: 'Manifest-Millenials',
       site: "https://manifestmillenials.herokuapp.com",
+      github: "https://github.com/kibagateaux/Manifest-Millennials",
       desc: `Social media platform for college students interested in business,
              finance, design, and technology. Schminop uses news APIs to supply
              information and topics for conversation on the forum.
              Designed, built, managed, and deployed to Heroku by myself.`,
       skills: [nodejs, JS, postgres]
-    }
+    },
+    {
+      head: 'Telescope',
+      site: "https://dry-hollows-37710.herokuapp.com/login",
+      github: "https://github.com/kibagateaux/project3",
+      desc: `A game based on Hot or Not for your narcissistic self.
+             Never know which picture to post to Facebook or Instagram?
+             Use Telescope to have your pictures voted on so you know
+             which one to post and get the most likes.
+             You upload the photos of yourself you want voted on to Imgur
+             and we pull them using Imgur's API.
+             View your profile to see your most liked and post it straight away.`,
+      skills: [RubyonRails, postgres, materialUI]
+    },
   ]
   const info = {
     filfil:{
@@ -144,7 +147,7 @@ const [blurb, filfil, ga, gsdo, nols, schminop, aboutme, close] = classes;
 //transitional popup from position to center
 //setTimeout for blurb func when transition complete from planet click function
 
-// const createBlurb = (event) => {
+// const addBlurb = (event) => {
 //    let posX = $(this).offset().left,
 //        posY = $(this).offset().top,
 //        x = event.pageX - posX/ 2,
@@ -166,6 +169,36 @@ const [blurb, filfil, ga, gsdo, nols, schminop, aboutme, close] = classes;
   // }).addClass("rippleEffect");
 // }
 
+const addBlurb = () => {
+
+  $('.blurb').remove()
+
+  $('body').append('<div class="blurb"> </div>');
+  $('.blurb').append('<div class="verbage"><div>');
+  $('.verbage').append(`<h4 class="head"></h4>`);
+  $('.blurb').append(`<img src="./images/icons/whiteX.png"class="close"/>`)
+
+  $('.close').on('click', () => $('.blurb').remove() )
+
+}
+
+
+const addProjects = () => {
+  projects.forEach((project) => {
+      $('.verbage').append(`<div id=${project.head} class="project-name">
+                              <a href=${project.site} target="_blank">
+                                ${project.head}
+                              </a>
+                            </div>`)
+      $(`#${project.head}`).append(`<p class="desc"> ${project.desc} </p>`)
+      $(`#${project.head}`).append('<div class="skills"> </div>')
+      project.skills.forEach((skill) => {
+        $(`#${project.head} .skills`).append(`<img src=${skill} class="skill">`)
+      })
+    })
+}
+
+
 const addTech = (obj) => {
   $('body').append('<div class="blurb"> </div>');
   $('.blurb').append('<div class="verbage"> <div>');
@@ -174,7 +207,7 @@ const addTech = (obj) => {
                           <span class="time"> ${obj.time} </span>
                         </h4>`);
   $('.verbage').append(`<p class="desc"> ${obj.desc} </p>`)
-  $('.blurb').append(`<img src="./images/skills/whiteX.png"class="close"/>`)
+  $('.blurb').append(`<img src="./images/icons/whiteX.png"class="close"/>`)
 
 
   // obj.skills.forEach(function(skill){
@@ -209,7 +242,7 @@ const addDesign = (obj) => {
                           <span class="time"> ${obj.time} </span>
                         </h4>`);
   $('.verbage').append(`<p class="desc"> ${obj.desc} </p>`)
-  $('.blurb').append(`<img src="./images/skills/whiteX.png"class="close"/>`)
+  $('.blurb').append(`<img src="./images/icons/whiteX.png"class="close"/>`)
   $('.verbage').append('<div class="skills"> </div>');
 
   obj.skills.forEach(function(skill){
@@ -262,10 +295,16 @@ $("div").click(function (e) {
 
 
 
-
+$('#projects').on('click', () => {
+  addBlurb();
+  addProjects();
+})
 
 
 $('#filfil').on('click', function(event){
+  console.log(event);
+   console.log(event.currentTarget.attributes.id)
+  console.log(event.currentTarget.attributes[1].nodeValue)
   $('.blurb').remove();
   // createBlurb(event);
   addDesign(info.filfil)
@@ -273,29 +312,39 @@ $('#filfil').on('click', function(event){
 });
 $('#ga').on('click', function(event){
   $('.blurb').remove();
+  console.log(event.currentTarget.attributes[1].nodeValue)
   // createBlurb(event);
+  console.log(event)
   addTech(info.ga)
   console.log("ga showing");
 });
 $('#gsdo').on('click', function(event){
   $('.blurb').remove();
+  console.log(event)
   // createBlurb(event);
+  console.log(event.currentTarget.attributes[1].nodeValue)
   addDesign(info.gsdo)
   console.log("gsdo showing");
 });
 $('#nols').on('click', function(event){
   $('.blurb').remove();
   // createBlurb(event);
+  console.log(event)
   addDesign(info.nols)
+  console.log(event.currentTarget.attributes[1].nodeValue)
   console.log("nols showing");
 });
 $('#aboutme').on('click', function(event){
   $('.blurb').remove();
   // createBlurb(event);
+  console.log(event.currentTarget.attributes[1].nodeValue)
+  console.log(event.currentTarget.attributes.id)
   addTech(info.me)
   console.log("aboutme showing");
 });
 
-
+// $('.planet').on('click', (event) => {
+//   console.log(event.currentTarget.attributes.id)
+// })
 
 })
